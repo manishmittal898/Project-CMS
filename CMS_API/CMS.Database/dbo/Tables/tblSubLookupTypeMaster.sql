53f3db6990f9d2ffc08bd08654b1fd0a6f31bbfb
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2022-06-16 14:59:23 +0530

    Changes

--- CMS_API/CMS.Database/dbo/Tables/tblSubLookupTypeMaster.sql
+++ CMS_API/CMS.Database/dbo/Tables/tblSubLookupTypeMaster.sql
@@ -2,6 +2,7 @@
 (
     [Id]         BIGINT            IDENTITY (1, 1) NOT NULL,
     [Name]       NVARCHAR (250) NULL,
+    [LookUpId]    BIGINT            NOT NULL,
     [CreatedBy]  BIGINT NOT NULL,
     [CreatedOn]  DATETIME       NOT NULL DEFAULT getdate(),
     [ModifiedBy] BIGINT NOT NULL,
@@ -9,6 +10,7 @@
     [IsActive]   BIT            NOT NULL DEFAULT 1,
     [IsDelete] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_tblSubLookupMaster] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [tblLookupMaster_LookUpId] FOREIGN KEY ([LookUpId]) REFERENCES [dbo].[tblLookupMaster] ([Id]),
     CONSTRAINT [FK_tblSubLookupTypeMaster_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [tblUserMaster]([UserId]),
     CONSTRAINT [FK_tblSubLookupTypeMaster_ModifiedBy] FOREIGN KEY ([ModifiedBy]) REFERENCES [tblUserMaster]([UserId])
 
