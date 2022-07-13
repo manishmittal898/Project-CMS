7f5b50110b6bf6e325cc49c5ea6fbbb6a7437d00
manishkumar.agarwal@dotsquares.com-at-180283404574 <manishkumar.agarwal@dotsquares.com>
2022-07-13 17:35:43 +0530

    design updates

---
+++ CMS_App/src/app/Content/Common/app-no-records/app-no-records.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-no-records',
+  templateUrl: './app-no-records.component.html',
+  styleUrls: ['./app-no-records.component.scss']
+})
+export class AppNoRecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- CMS_App/src/environments/environment.ts
+++ CMS_App/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   AESKey: "8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@",
-  apiEndPoint: "http://localhost:31958/api/",
- // apiEndPoint :"http://api.storeone.co.in/api/",
+  // apiEndPoint: "http://localhost:31958/api/",
+ apiEndPoint :"http://api.storeone.co.in/api/",
 
-  IsAutoLogin: true
+  IsAutoLogin: false
 };
