2b8956c552fb7ab17d706ec75d0fd41caf8cc214
manishkumar.agarwal@dotsquares.com-at-180283404574 <manishkumar.agarwal@dotsquares.com>
2022-07-14 16:42:11 +0530

    design updates

--- CMS-Frontent/src/app/app.component.ts
+++ CMS-Frontent/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{Tooltip} from "node_modules/bootstrap/dist/js/bootstrap.esm.min.js";
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CMS-Frontent';
+
+
+
+  constructor() { }
+  ngOnInit(): void {
+    Array.from(document.querySelectorAll('button[data-bs-toggle="tooltip"]'))
+    .forEach(tooltipNode => new Tooltip(tooltipNode))
+  }
+
+
 
 }
