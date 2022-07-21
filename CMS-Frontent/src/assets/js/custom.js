34ef9d914988231f04bf89ab882915b55215dbfd
manishkumar.agarwal@dotsquares.com-at-180283404574 <manishkumar.agarwal@dotsquares.com>
2022-07-21 16:01:56 +0530

    design updates

--- CMS-Frontent/src/assets/js/custom.js
+++ CMS-Frontent/src/assets/js/custom.js
@@ -68,3 +68,20 @@ $(window).scroll(function() {
         $("header").removeClass("header-fix");
     }
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+    everydropdown.addEventListener('shown.bs.dropdown', function () {
+        el_overlay = document.createElement('span');
+        el_overlay.className = 'screen-darken';
+        document.body.appendChild(el_overlay)
+    });
+
+    everydropdown.addEventListener('hide.bs.dropdown', function () {
+      document.body.removeChild(document.querySelector('.screen-darken'));
+    });
+  });
+
+});
