2d799ba3f9379e445894b8333491b714cc998cfb
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2022-08-27 14:36:24 +0530

    Changes

--- CMS-Frontent/src/app/Pages/html-pages/product-list/product-detail/category-product-list/category-product-list.component.ts
+++ CMS-Frontent/src/app/Pages/html-pages/product-list/product-detail/category-product-list/category-product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ProductFilterModel, ProductMasterViewModel, ProductService } from 'src/app/Shared/Services/product.service';
-
+declare var $: any;
 @Component({
   selector: 'app-category-product-list',
   templateUrl: './category-product-list.component.html',
@@ -9,11 +9,14 @@ import { ProductFilterModel, ProductMasterViewModel, ProductService } from 'src/
 export class CategoryProductListComponent implements OnInit, OnChanges {
   @Input() CategoryId: number;
   @Input() SubCategoryId: number;
-  @Input() ExcludeId: number=0;
+  @Input() ExcludeId: number = 0;
 
   indexModel = new ProductFilterModel();
   model: ProductMasterViewModel[] = [];
   totalRecords: number = 0;
+  get productModel() {
+    return this.model.filter(x => x.Id != this.ExcludeId);
+  }
   constructor(private readonly _productService: ProductService) {
 
     this.indexModel.CategoryId = [Number(this.CategoryId)];
@@ -21,9 +24,10 @@ export class CategoryProductListComponent implements OnInit, OnChanges {
   }
   ngOnChanges(changes: SimpleChanges): void {
 
-    if (changes && changes.CategoryId.currentValue != changes.CategoryId.previousValue) {
+    if (changes && changes?.CategoryId?.currentValue != changes?.CategoryId?.previousValue) {
       this.getList();
     }
+
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
 
@@ -38,14 +42,56 @@ export class CategoryProductListComponent implements OnInit, OnChanges {
       this._productService.GetList(this.indexModel).subscribe(response => {
         debugger
         if (response.IsSuccess) {
-          this.model = response.Data.filter(x => x.Id != this.ExcludeId);
+          this.model = response.Data;
           this.totalRecords = (Number(response.TotalRecord) > 0 ? response.TotalRecord - (this.ExcludeId > 0 ? 1 : 0) : 0) as number;
-
+          this.AddSlider();
         }
       });
     }
 
   }
 
+  AddSlider() {
+    $('.slider-items-4').slick({
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [{
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ]
+    });
+  }
 
 }
--- CMS-Frontent/src/app/Pages/html-pages/product-list/product-detail/product-detail.component.ts
+++ CMS-Frontent/src/app/Pages/html-pages/product-list/product-detail/product-detail.component.ts
@@ -53,10 +53,10 @@ export class ProductDetailComponent implements OnInit {
 
         this.shareLink = this._sainitizer.bypassSecurityTrustResourceUrl(`whatsapp://send?text=${environment.sitePath}/product/${this.model.Name.split(' ').join('-')}/${this.recordId}
          `)
-        this.AddSlider();
+
         setTimeout(() => {
           this.ProductDetailSlider()
-        }, 50);
+        }, 100);
 
       }
     })
@@ -64,7 +64,7 @@ export class ProductDetailComponent implements OnInit {
   }
 
   ProductDetailSlider() {
-    $('.product-d-main-slider').slick({
+    $('.product-d-main-slider')?.slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: true,
@@ -75,7 +75,7 @@ export class ProductDetailComponent implements OnInit {
       autoplaySpeed: 4000,
       asNavFor: '.product-d-main-slider-nav'
     });
-    $('.product-d-main-slider-nav').slick({
+    $('.product-d-main-slider-nav')?.slick({
       slidesToShow: 5,
       slidesToScroll: 1,
       arrows: false,
@@ -88,48 +88,6 @@ export class ProductDetailComponent implements OnInit {
       focusOnSelect: true
     });
   }
-  AddSlider() {
-    $('.slider-items-4').slick({
-      dots: true,
-      infinite: true,
-      speed: 1000,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [{
-        breakpoint: 1600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      ]
-    });
 
-  }
 
 }
