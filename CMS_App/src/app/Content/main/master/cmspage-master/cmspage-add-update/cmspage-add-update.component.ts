2ee71c198730cdaab5695d5a6a8451ca2334405d
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2022-09-25 12:02:58 +0530

    Changes

--- CMS_App/src/app/Content/main/master/cmspage-master/cmspage-add-update/cmspage-add-update.component.ts
+++ CMS_App/src/app/Content/main/master/cmspage-master/cmspage-add-update/cmspage-add-update.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { EditorConfig } from './../../../../../Shared/Helper/constants';
 import { CMSPageMasterService } from 'src/app/Shared/Services/cmspage-master.service';
 import { CommonService } from 'src/app/Shared/Services/common.service';
 
@@ -12,7 +14,16 @@ import { CommonService } from 'src/app/Shared/Services/common.service';
 export class CMSPageAddUpdateComponent implements OnInit {
   id: number = 0;
   pageName: string = '';
-  constructor(private _router: Router, private _activatedRoute: ActivatedRoute, private readonly _commonService: CommonService,
+  model={} as { Heading: string, Content: string };
+  editorConfig = EditorConfig.Config;
+  formgrp = this.fb.group({
+    Heading: [undefined, Validators.required],
+    Content: [undefined, Validators.required],
+
+  });
+  get f() { return this.formgrp.controls; }
+
+  constructor(private readonly fb: FormBuilder, private _router: Router, private _activatedRoute: ActivatedRoute, private readonly _commonService: CommonService,
     private readonly toast: ToastrService, private _cmsPageService: CMSPageMasterService,) {
     debugger
 
