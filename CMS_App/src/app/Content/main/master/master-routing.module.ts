68ad560dcb4e715c9919616d7235496c5ad45fcb
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2022-10-10 21:51:12 +0530

    chnges

--- CMS_App/src/app/Content/main/master/master-routing.module.ts
+++ CMS_App/src/app/Content/main/master/master-routing.module.ts
@@ -7,6 +7,15 @@ import { LookupTypeComponent } from "./lookup-type/lookup-type.component";
 import { LookupsAddEditComponent } from "./lookup-type/lookups/lookups-add-edit/lookups-add-edit.component";
 import { LookupsComponent } from "./lookup-type/lookups/lookups.component";
 import { SubLookupComponent } from "./lookup-type/lookups/sub-lookup/sub-lookup.component";
+import { GeneralEntryMasterComponent } from './general-entry-master/general-entry-master.component';
+import { GeneralEntryMasterAddEditComponent } from './general-entry-master/general-entry-master-add-edit/general-entry-master-add-edit.component';
+import { GeneralEntryMasterDetailComponent } from './general-entry-master/general-entry-master-detail/general-entry-master-detail.component';
+import { GeneralEntryCategoryMasterAddEditComponent } from "./general-entry-master/general-entry-category-master/general-entry-category-master-add-edit/general-entry-category-master-add-edit.component";
+import { GeneralEntryCategoryMasterDetailComponent } from "./general-entry-master/general-entry-category-master/general-entry-category-master-detail/general-entry-category-master-detail.component";
+import { GeneralEntryCategoryMasterComponent } from "./general-entry-master/general-entry-category-master/general-entry-category-master.component";
+import { GeneralEntrySubCategoryMasterAddEditComponent } from "./general-entry-master/general-entry-category-master/general-entry-sub-category-master/general-entry-sub-category-master-add-edit/general-entry-sub-category-master-add-edit.component";
+import { GeneralEntrySubCategoryMasterDetailComponent } from "./general-entry-master/general-entry-category-master/general-entry-sub-category-master/general-entry-sub-category-master-detail/general-entry-sub-category-master-detail.component";
+import { GeneralEntrySubCategoryMasterComponent } from "./general-entry-master/general-entry-category-master/general-entry-sub-category-master/general-entry-sub-category-master.component";
 
 const routes: Routes = [
   { path: '', component: LookupTypeComponent },
@@ -29,6 +38,21 @@ const routes: Routes = [
   { path: 'lookup/:name/update/:id', component: LookupsAddEditComponent },
 
 
+  { path: 'general-entry', component: GeneralEntryMasterComponent },
+  { path: 'general-entry/add', component: GeneralEntryMasterAddEditComponent },
+  { path: 'general-entry/edit', component: GeneralEntryMasterAddEditComponent },
+  { path: 'general-entry/detail', component: GeneralEntryMasterDetailComponent },
+
+  { path: 'general-entry-category', component: GeneralEntryCategoryMasterComponent },
+  { path: 'general-entry-category/add', component: GeneralEntryCategoryMasterAddEditComponent },
+  { path: 'general-entry-category/edit', component: GeneralEntryCategoryMasterAddEditComponent },
+  { path: 'general-entry-category/detail', component: GeneralEntryCategoryMasterDetailComponent },
+
+
+  { path: 'general-entry-sub-category', component: GeneralEntrySubCategoryMasterComponent },
+  { path: 'general-entry-sub-category/add', component: GeneralEntrySubCategoryMasterAddEditComponent },
+  { path: 'general-entry-sub-category/edit', component: GeneralEntrySubCategoryMasterAddEditComponent },
+  { path: 'general-entry-sub-category/detail', component: GeneralEntrySubCategoryMasterDetailComponent },
 
   // {path:'SubLookupType',component: SubLookupTypeComponent},
   // {path:'SubLookupTypeAddEdit',component: SubLookupTypeAddEditComponent},
