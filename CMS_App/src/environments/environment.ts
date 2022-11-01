a879808021ff4ab4ddaeefa6cafd061bdf1ebe86
manishkumar.agarwal@dotsquares.com-at-180283404574 <manishkumar.agarwal@dotsquares.com>
2022-11-01 17:54:05 +0530

    design updates

--- CMS_App/src/environments/environment.ts
+++ CMS_App/src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
   AESKey: "8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@",
  //  apiEndPoint: "http://localhost:31958/api/",
   apiEndPoint :"https://api.storeone.co.in/api/",
-
   IsAutoLogin: false
 };
