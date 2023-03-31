93c7a6ee107b5820fc00607302f04936d9af52c3
manishmittal898 <ma578811@gmail.com>
2023-03-31 23:36:49 +0530

    design updates

--- CMS-Frontent/src/environments/environment.ts
+++ CMS-Frontent/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   AESKey: "0123456789abcdef0123456789abcdef",
-   apiEndPoint: "http://localhost:31958/api/",
-  // apiEndPoint :"https://api.storeone.co.in/api/",
+  //  apiEndPoint: "http://localhost:31958/api/",
+  apiEndPoint :"https://api.storeone.co.in/api/",
   sitePath: 'https://demo.storeone.co.in/',
   IsAutoLogin:false
 };
