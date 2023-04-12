646cb9fc83339fbb71abe0c8ad3d9e5e1b93eeb3
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2023-04-12 14:54:19 +0530

    user component

--- CMS_App/src/app/Content/main/user/user.module.ts
+++ CMS_App/src/app/Content/main/user/user.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { SharedModule } from 'src/app/Shared/Helper/shared/shared.module';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserAddEditComponent } from './user-add-edit/user-add-edit.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    UserDetailComponent,
+    UserAddEditComponent
   ],
   imports: [
     CommonModule,
