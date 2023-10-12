7032370be7448e2a0d188d47edb478d7c36c3de5
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2023-10-12 22:56:13 +0530

    page shwo hide option

--- CMS_App/src/app/Content/main/master/general-entry-master/general-entry-master-detail/general-entry-master-detail.component.ts
+++ CMS_App/src/app/Content/main/master/general-entry-master/general-entry-master-detail/general-entry-master-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-general-entry-master-detail',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-entry-master-detail.component.scss']
 })
 export class GeneralEntryMasterDetailComponent implements OnInit {
+  id: string = "";
+  @Input() set Id(value: string) {
+    this.id = value;
+   // this.getDetail();
+  }
+  @Input() refreshData: boolean = false;
 
   constructor() { }
 
