8a8df0778ed1801e1d1e8619eabfad660b0eb6dd
iamthesandeepsuthar <sandeep.suthar08@gmail.com>
2023-11-03 00:39:02 +0530

    chnges

--- CMS-Frontent/src/app/Shared/Page/login/user-password/user-password.component.ts
+++ CMS-Frontent/src/app/Shared/Page/login/user-password/user-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-password',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPasswordComponent implements OnInit {
   step = "step1"
-  constructor() { }
+
+
+  Email = new FormControl('', [Validators.required, Validators.email])
+  OTP = new FormControl([undefined, Validators.required])
+  Password = new FormControl(['', [Validators.required, Validators.minLength(8)]],)
+  ConfirmPassword = new FormControl(['', [Validators.required, this.passwordMatchValidator]])
+
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
@@ -19,10 +27,30 @@ export class UserPasswordComponent implements OnInit {
 
   }
   sendOTP() {
-    this.step = 'step2'
+    this.Email.markAsTouched();
+    if (!this.Email.invalid) {
+
+      this.step = 'step2'
+    }
   }
   verifyOTP() {
     this.step = 'step3'
+
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    if (control && control.value !== undefined && control.value !== null && control.value !== '') {
+      const confirmPassword = control.value;
+      const passwordControl = control.root.get('Password');
+      if (passwordControl) {
+        const password = passwordControl.value;
+        if (confirmPassword !== password) {
+          return { notMatched: true };
+        }
+      }
+    }
+    return null;
+
 
   }
 }
